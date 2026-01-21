227c42a8d0a6fe110a7b94fc53995e1f8b674356
Руслан Муллаянов <vvmrivv@gmail.com>
2026-01-21 15:36:51 +0500

    внесены все исправления после проверок flake8,mypy,black, isort

--- src/masks.py
+++ src/masks.py
@@ -6,7 +6,7 @@ def get_mask_card_number(card_number: int) -> str:
     7000 79** **** 6361 - выход функции
     """
     number_musk: str = str(card_number).replace(str(card_number)[6:12], "*" * len(str(card_number)[6:12]))
-    done_card_number: str = " ".join([number_musk[i : i + 4] for i in range(0, len(number_musk), 4)])
+    done_card_number: str = " ".join([number_musk[i: i + 4] for i in range(0, len(number_musk), 4)])
     return done_card_number
 
 
--- src/widget.py
+++ src/widget.py
@@ -8,18 +8,22 @@ def mask_account_card(type_number_or_account: str) -> str:
     """
     if "Счет" in type_number_or_account:
         from src.masks import get_mask_account
-        account_done_result: str = get_mask_account(int(type_number_or_account[-20: ]))
-        return f'Счет {account_done_result}'
+
+        account_done_result: str = get_mask_account(int(type_number_or_account[-20:]))
+        return f"Счет {account_done_result}"
     else:
         from src.masks import get_mask_card_number
-        return type_number_or_account.replace(type_number_or_account[-16:],get_mask_card_number(int(type_number_or_account[-16:])))
+
+        return type_number_or_account.replace(
+            type_number_or_account[-16:], get_mask_card_number(int(type_number_or_account[-16:]))
+        )
 
 
-def get_date(date: str):
+def get_date(date: str) -> str:
     """
     Функция принимает на вход дату в формате:
     "2024-03-11T02:26:18.671407"
     и возвращает строку с датой в формате:
     "ДД.ММ.ГГГГ" ("11.03.2024")
     """
-    return f'{date[8:10]}.{date[5:7]}.{date[0:4]}'
+    return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
