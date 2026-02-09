dda6a1671ae6edc1321cd75a49aa2cdc7b49e57a
Mullayanov <vvmrivv@gmail.com>
2026-02-09 21:48:39 +0500

    Внесены все данные в модуль main.py, для проверки ДЗ 9.1/9.2/10.1

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from src.masks import get_mask_account, get_mask_card_number
 from src.processing import filter_by_state, sort_by_date
 from src.widget import get_date, mask_account_card
 
+# Проверка ДЗ 9.1
+print("\nПроверяем домашнее задание 9.1\n")
+print(get_mask_card_number(7000792289606361))
+print(get_mask_account(73654108430135874305))
+
+print("\nПроверяем домашнее задание 9.2\n")
 # Список вводных данных для проверки ДЗ 9.2
 list_arguments = [
     "Maestro 1596837868705199",
@@ -17,6 +24,8 @@ for i in list_arguments:
     print(mask_account_card(i))
 # Проверка работы функции get_date
 print(get_date("2024-03-11T02:26:18.671407"))
+
+print("\nПроверяем домашнее задание 10.1\n")
 # Список вводных данных для проверки ДЗ 10.1
 list_dict = [
     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
