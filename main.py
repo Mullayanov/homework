2dea3b5baa6e3fd9b542196d6a85d225502bf2c3
Mullayanov <vvmrivv@gmail.com>
2026-02-09 21:06:04 +0500

    Проверка проекта с помощью flake8, isort, black, mypy

--- main.py
+++ main.py
@@ -18,12 +18,14 @@ for i in list_arguments:
 # Проверка работы функции get_date
 print(get_date("2024-03-11T02:26:18.671407"))
 # Список вводных данных для проверки ДЗ 10.1
-list_dict = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-             {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-             {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-             {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+list_dict = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
 # Проверка работы функции filter_by_state
-print(filter_by_state(list_dict, 'canceled'))
+print(filter_by_state(list_dict, "canceled"))
 # Проверка работы функции sort_by_date.
 # Для сортировки по убыванию, второй аргумент не указываем, либо указываем True.
 # Для сортиировки по возрастанию, второй аргумент указываем False.
